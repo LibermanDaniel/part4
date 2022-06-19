93725df0ce2b3bd2740753e7a16eae16fe0eea11
Daniel Liberman <liberman.daniel@outlook.com>
2022-06-19 22:11:55 +0300

    userExtractor middleware added

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,7 +2,6 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 require('express-async-errors')
-const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user')
@@ -12,14 +11,7 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token missing or invalid' })
-  }
-
-  console.log(body)
-  const user = await User.findById(decodedToken.id)
-  console.log(user)
+  const user = await User.findById(request.user.id)
 
   if (!(body.likes)) {
     body.likes = 0
@@ -56,12 +48,15 @@ blogsRouter.get('/:id', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndRemove(request.params.id)
-  response.status(204).end()
-})
-
-blogsRouter.delete('/:id', async (request, response) => {
-  if (request.params.id) {
+  if (!request.params.id) {
+    response.status(404).json({ error: 'Missing id!' })
+  }
+  const user = await User.findById(request.user.id)
+  const blog = await Blog.findById(request.params.id)
+  if (!blog) {
+    response.status(404).json({ error: 'Could not find the requested blogpost' })
+  }
+  if (blog.user._id.toString() === user._id.toString()) {
     await Blog.findByIdAndRemove(request.params.id)
     response.status(204).end()
   }
